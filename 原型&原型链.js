b28e09dda534909ac9a9b18e56cea312040c277a
crystalcici <517455797@qq.com>
2020-04-13 17:28:34 +0800

    4 13

---
+++ 原型&原型链.js
@@ -0,0 +1,35 @@
+// 父类
+class People {
+    constructor(name) {
+        this.name = name
+    }
+    eat() {
+        console.log(`${this.name} eat something`)
+    }
+}
+
+// 子类
+class Student extends People {
+    constructor(name, number) {
+        super(name)
+        this.number = number
+    }
+    sayHi() {
+        console.log(`姓名 ${this.name} 学号 ${this.number}`)
+    }
+}
+
+// 子类
+class Teacher extends People {
+    constructor(name, major) {
+        super(name)
+        this.major = major
+    }
+    teach() {
+        console.log(`${this.name} 教授 ${this.major}`)
+    }
+}
+
+// 实例
+const xialuo = new Student('夏洛', 100)
+const wanglaoshi = new Teacher('王老师', '语文')
