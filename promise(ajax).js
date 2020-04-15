ab364a23502ecf02bbd059805991e123a085cd76
crystalcici <517455797@qq.com>
2020-04-15 11:10:42 +0800

    4 15

---
+++ promise(ajax).js
@@ -0,0 +1,17 @@
+const getJson = function (url) {
+    return new Promise((resolve,reject) => {
+        const xhr = new XMLHttpRequest(){
+            xhr.open('GET','url',true);
+            xhr.onreadystatechange=function () {
+                if(this.readyState===4&&this.status===200){
+                    resolve()
+                }
+                else{
+                    const resJson = {code:this.status,reponse:this.response};
+                    reject(resJson)
+                }
+            }
+        };
+        xhr.send()
+    })
+};
