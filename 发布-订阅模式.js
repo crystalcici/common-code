c23a69e7108c6603039cdbd82e73f2e620258882
crystalcici <517455797@qq.com>
2020-09-06 11:12:14 +0800

    feat(9 6)

--- 发布-订阅模式.js
+++ 发布-订阅模式.js
@@ -1,35 +1,21 @@
-let event = {
-    clientList: {},
-    listen (key, fn) {
-        if(!this.clientList[key]){
-            this.clientList[key] = [];
+class PubSub {
+    constructor() {
+        this.subscribers = {}
+    }
+    subscribe (type, fn) {
+        if (!Object.prototype.hasOwnProperty.call(this.subscribers, type)) {
+            this.subscribers[type] = [];
         }
-        this.clientList[key].push(fn)  // 订阅的消息存入缓存列表
-    },
-    trigger (type, money) {
-        let fns = this.clientList[type]
-        if(!fns || fns.length ===0){ // 未绑定消息
-            return false
-        }
-        fns.forEach(fn =>{
-            fn.apply(this,[money])
-        })
+        this.subscribers[type].push(fn);
+    }
+    unsubscribe (type, fn) {
+        let listeners = this.subscribers[type];
+        if (!listeners || !listeners.length) return;
+        this.subscribers[type] = listeners.filter(v => v !== fn);
+    }
+    publish (type, ...args) {
+        let listeners = this.subscribers[type];
+        if (!listeners || !listeners.length) return;
+        listeners.forEach(fn => fn(...args));
     }
 }
-
-//定义installEvent函数，用于给多有对象动态安装发布订阅功能
-
-let installEvent = obj => {
-    for(let i in event) {
-        obj[i] = event[i]
-    }
-}
-
-// 给售楼处对象salesOffices动态增加发布-订阅功能
-let salesOffices = {};
-installEvent(salesOffices);
-
-salesOffices.listen('square88',price =>{
-    console.log(`xxxx,价格=`+price)
-})
-salesOffices.trigger('squareMeter88', 2000000)
